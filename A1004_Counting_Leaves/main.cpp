f46ab201c7dfd57943fb9b4b9c16a5e3fe437286
hejiangda <hejiangda@163.com>
2019-10-22 21:07:38 +0800

    用链表表示树。树通过 dfs 访问结点后判断是否是叶子结点 更新 book 中的结点数，同时更新树高度

---
+++ A1004_Counting_Leaves/main.cpp
@@ -0,0 +1,45 @@
+// 给出一棵家谱树，找出每一层中没有孩子的结点个数
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v[100];
+// book 记录每层没有孩子结点的结点个数
+int book[100],maxdepth=-1;
+void dfs(int index,int depth){
+    // 如果 index 结点没有孩子
+    if(v[index].size()==0){
+        // 该结点所在层的结点个数加一
+        book[depth]++;
+        // 更新树的最大层数
+        maxdepth = max(maxdepth,depth);
+        return ;
+    }
+    // 对于每个结点，遍历其字节点
+    for(int i=0;i<v[index].size();i++)
+        // 遍历子结点时深度加一
+        dfs(v[index][i],depth+1);
+}
+
+int main(){
+    int n,m,k,node,c;
+    // n 结点总数, m 非空结点个数
+    scanf("%d %d",&n,&m);
+    // 读入 m 个非空结点的 k 个孩子结点
+    for(int i=0;i<m;i++){
+        scanf("%d %d",&node,&k);
+        // 将孩子结点存放在链表中
+        for(int j=0;j<k;j++){
+            scanf("%d",&c);
+            v[node].push_back(c);
+        }
+    }
+    // 遍历第一个结点，深度为零
+    dfs(1,0);
+    printf("%d",book[0]);
+    // 输出每层没有孩子结点的结点个数
+    for(int i=1;i<=maxdepth;i++)
+        printf(" %d",book[i]);
+    return 0;
+}
--- PAT.pro
+++ PAT.pro
@@ -3,4 +3,5 @@ TEMPLATE = subdirs
 SUBDIRS += \
     A1001_A_Add_B_Format \
     A1002_A_Add_B_for_Polynomials \
-    A1003_Emergency
+    A1003_Emergency \
+    A1004_Counting_Leaves
