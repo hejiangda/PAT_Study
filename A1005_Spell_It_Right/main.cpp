ca23012fc5ecaa9dd7c9f8022fb21bfa37157060
hejiangda <hejiangda@163.com>
2019-10-23 19:21:01 +0800

    给一个非负正数N,计算N的每一位相加的和,然后输出和的每一位的英文读音

---
+++ A1005_Spell_It_Right/main.cpp
@@ -0,0 +1,18 @@
+// 给出一个非负整数，对每一位求和，用英文按位输出结果
+//
+#include <iostream>
+using namespace std;
+int main(){
+    string a;
+    cin >> a;
+    int sum =0;
+    for(int i=0;i<a.length(),i++)
+        sum+=(a[i]-'0');
+    // to_string()将其他类型转化为字符串
+    string s = to_string(sum);
+    string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    cout<<arr[s[0]-'0'];
+    for(int i=1;i<s.length();i++)
+        cout<<" "<<arr[s[i]-'0'];
+    return 0;
+}
--- PAT.pro
+++ PAT.pro
@@ -4,4 +4,5 @@ SUBDIRS += \
     A1001_A_Add_B_Format \
     A1002_A_Add_B_for_Polynomials \
     A1003_Emergency \
-    A1004_Counting_Leaves
+    A1004_Counting_Leaves \
+    A1005_Spell_It_Right
